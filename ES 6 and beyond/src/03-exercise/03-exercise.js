515977e9aa45766a30f42d3747d461a00c17010a
diegok23 <dkogut@gmail.com>
2022-04-20 20:09:51 +0200

    Resuelto

--- ES 6 and beyond/src/03-exercise/03-exercise.js
+++ ES 6 and beyond/src/03-exercise/03-exercise.js
@@ -25,7 +25,9 @@
  */
 
 // Finish the code of the function
-const exercise03 = () => {};
+const exercise03 = (first,second,...all) => {
+    return all
+};
 
 // Don’t change the code bellow this line
 export default exercise03;
